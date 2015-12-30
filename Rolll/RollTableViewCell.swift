5909d06e961b339aaa6db304d067c04c86fcf399
Miles Ranisavljevic <itstop3@gmail.com>
2015-12-29 20:38:38 -0800

    Add homeVC and table view with custom xib and cell class.

---
+++ Rolll/RollTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RollTableViewCell.swift
+//  Rolll
+//
+//  Created by Miles Ranisavljevic on 12/29/15.
+//  Copyright © 2015 creeperspeak. All rights reserved.
+//
+
+import UIKit
+
+class RollTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
