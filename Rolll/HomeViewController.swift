1329094a6b300377198e4e8641459f5c1a0dc70a
Miles Ranisavljevic <itstop3@gmail.com>
2015-12-29 21:42:21 -0800

    add NSFetchedResultsController delegate methods with comments to lead next steps.

--- Rolll/HomeViewController.swift
+++ Rolll/HomeViewController.swift
@@ -19,7 +19,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setUpFetchedResultController()
+//        self.setUpFetchedResultController()
         self.setUpTableView()
     }
 
@@ -36,6 +36,9 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         self.fetchedResultsController = NSFetchedResultsController(fetchRequest: rollFetchRequest, managedObjectContext: NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.MainQueueConcurrencyType), sectionNameKeyPath: nil, cacheName: nil)
         guard let controller = self.fetchedResultsController else { return }
         controller.delegate = self
+        do {
+            try controller.performFetch()
+        } catch {}
     }
     
     func setUpTableView() {
@@ -47,8 +50,24 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
    //MARK: NSFetchedResultsControllerDelegate
     
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        self.rollTableView.beginUpdates()
+    }
+    
     func controller(controller: NSFetchedResultsController, sectionIndexTitleForSectionName sectionName: String) -> String? {
         return sectionName
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        //switch on type to deal with insertions/deletions
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        //switch on type and deal with insertions/deletions/updates/moves
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        self.rollTableView.endUpdates()
     }
     
     //MARK: UITableViewDelegate, UITableViewDataSource
